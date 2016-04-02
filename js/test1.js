fe5c5feddedbc86a2ff8314d5ccc17736a55df08
skyguy99 <coolsnt@gmail.com>
2016-04-01 22:16:14 -0700

    m.,

---
+++ js/test1.js
@@ -0,0 +1,10 @@
+
+var word;
+function goToProject(){
+    word = document.getElementById("project").value;
+    //window.location.href = "http://mysite.com/"+project;
+
+    alert("Hello "+word);
+    context.fillText(word,10,50);
+
+};
