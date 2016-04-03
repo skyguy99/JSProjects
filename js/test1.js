d455f5b0093cf1936a39beca7945ab0a4de8a82c
skyguy99 <coolsnt@gmail.com>
2016-04-03 00:13:39 -0700

    m,.

--- js/test1.js
+++ js/test1.js
@@ -1,11 +1,3 @@
-var word;
 
-function goToProject(){
-    word = document.getElementById("project").value;
-    //window.location.href = "http://mysite.com/"+project;
-
-    alert("Hello "+word);
-
-};
 
 
